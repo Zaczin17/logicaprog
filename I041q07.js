d34af2329a6ac0ca73d92c842f4720f3dd06d662
NATHAN PEDREIRA FERREIRA <napedfer@gmail.com>
2026-06-17 09:53:53 -0300

    l

---
+++ I041q07.js
@@ -0,0 +1,13 @@
+let n1, n2,  n3, media 
+
+console.log("Informe o primeiro numero: ")
+n1 = 5;
+onsole.log("Informe o segundo numero: ")
+n1 = 7;
+onsole.log("Informe o terceiro numero: ")
+n1 = 9;
+
+media = ( n1 + n2 + n3 )  / 3; 
+console.log (`A media e ${media}`);
+
+//comentario  realizado em 17/06/26
