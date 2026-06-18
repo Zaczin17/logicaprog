6ad5857cf3d15e9064adc60d40b9a08632889820
NATHAN PEDREIRA FERREIRA <napedfer@gmail.com>
2026-06-18 11:46:47 -0300

    Continuaçao dos Exercicios Pt:2

---
+++ I051q01.js
@@ -0,0 +1,9 @@
+/* Desenvolver um programa que apresente todos os valores numéricos inteiros pares situados na faixa de 100 a
+200. */  
+ let cont = 100;
+
+do {
+     console.log( cont );      // % = resto da divisao
+   cont = cont + 1;
+} while ( cont <= 200);
+
